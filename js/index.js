bf8bdbb43510c015e4772923a7f9b903c4d8f8a7
eznrr <eznr527@sempreceub.com>
2024-09-02 10:26:00 -0300

    primeira versao de controle de ponto

---
+++ js/index.js
@@ -0,0 +1,60 @@
+navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude)
+});
+
+const diaSemana = document.getElementById("dia-semana");
+const dataAtual = document.getElementById("data-atual");
+const horaAtual = document.getElementById("hora-atual");
+const pontoDialog = document.getElementById("dialog-ponto");
+const openDialog = document.getElementById("btn-registrar-ponto");
+
+openDialog.addEventListener("click", register);
+
+diaSemana.textContent = getWeekDay();
+dataAtual.textContent = getCurrentDate();
+
+const dateDialog = document.getElementById("btn-dialog-data");
+dateDialog.textContent = getCurrentDate();
+
+const hourDialog = document.getElementById("btn-dialog-hora");
+hourDialog.textContent = getCurrentTime();
+
+const closeDialog = document.getElementById("btn-dialog-close");
+closeDialog.addEventListener("click", () => {
+    pontoDialog.close();
+})
+
+function register() {
+    pontoDialog.showModal();
+}
+
+function updateContentHour() {
+    horaAtual.textContent = getCurrentTime();
+}
+
+function getCurrentTime() {
+    const date = new Date();
+    return String(date.getHours()).padStart(2, '0') + ":" + String(date.getMinutes()).padStart(2, '0') + ":" + String(date.getSeconds()).padStart(2, '0');
+}
+
+function getCurrentDate() {
+    const date = new Date(); 
+    let mes = date.getMonth() + 1;
+    return String(date.getDate()).padStart(2, '0') + "/" + String(mes).padStart(2, '0') + "/" +  String(date.getFullYear()).padStart(2, '0');
+}
+
+function getWeekDay() {
+    const date = new Date()
+    const day = date.getDay()
+    const daynames = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return daynames[day]
+}
+
+updateContentHour();
+setInterval(updateContentHour, 1000);
+
+console.log(getCurrentTime());
+console.log(getCurrentDate());
+console.log(getWeekDay());
